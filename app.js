d9da5e28a8bd2d38d19ec45d4f2c9593f108e71d
니꼬 <nico@nomadcoders.co>
2022-07-01 17:02:56 +0900

    1.2 Paths

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(50, 50, 100, 200);
+ctx.rect(50, 50, 100, 100);
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(350, 350, 100, 100);
+ctx.rect(450, 450, 100, 100);
+ctx.fillStyle = "red";
+ctx.fill();
